f879a52409d321ea9dc1cc29610dc8dd3c031384
Khiem <khiemhuynh952@gmail.com>
2023-04-15 20:40:47 -0500

    update api call

--- src/chatbotConfig.js
+++ src/chatbotConfig.js
@@ -1,18 +1,5 @@
 import { createChatBotMessage } from "react-chatbot-kit";
-import Chat from "./widgets/Chat";
-// import Overview from "./widgets/Overview";
-// import GlobalStatistics from "./widgets/GlobalStatistics";
-// import LocalStatistics from "./widgets/LocalStatistics";
-// import Contact from "./widgets/Contact";
-// import MedicineDelivery from "./widgets/MedicineDelivery";
 import CoBotAvatar from "./CoBotAvatar";
-
-
-
-
-
-
-
 
 const config = {
   lang: "no",
@@ -28,50 +15,11 @@ const config = {
   initialMessages: [
     createChatBotMessage(
       `Hi, I'm here to provide you with latest COVID 19 data to keep you safe!`
-    ),
-    createChatBotMessage(
-      "Please start chatting with me about your health problems",
-      {
-        withAvatar: false,
-        delay: 400,
-        widget: "chat"
-      }
     )
   ],
   state: {},
   customComponents: { botAvatar: (props) => <CoBotAvatar {...props} /> },
-  widgets: [
-    // {
-    //   widgetName: "chat",
-    //   widgetFunc: async (props) => {
-    //     if (props.messages)
-    //       await getChatResponse(props.messages);
-    //     // Your chat widget logic goes here, using the response from the API call
-    //   },
-    //   mapStateToProps: ["messages"]
-    // }
-    {
-      widgetName: "chat",
-      widgetFunc: (props) => <Chat {...props} />,
-      mapStateToProps: ["messages"]
-    }//,
-    // {
-    //   widgetName: "globalStatistics",
-    //   widgetFunc: (props) => <GlobalStatistics />
-    // },
-    // {
-    //   widgetName: "localStatistics",
-    //   widgetFunc: (props) => <LocalStatistics />
-    // },
-    // {
-    //   widgetName: "emergencyContact",
-    //   widgetFunc: (props) => <Contact />
-    // },
-    // {
-    //   widgetName: "medicineDelivery",
-    //   widgetFunc: (props) => <MedicineDelivery />
-    // }
-  ]
+  widgets: []
 };
 
 export default config;
